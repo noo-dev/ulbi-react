20fbf25de02e147b94f29fb641317aafef48ba59
noo-dev <noo.dev23@gmail.com>
2024-01-10 21:17:38 +0500

    animantions implemented

--- src/components/PostList.jsx
+++ src/components/PostList.jsx
@@ -1,4 +1,5 @@
 import PostItem from "./PostItem";
+import { CSSTransition, TransitionGroup } from 'react-transition-group'
 
 const PostList = ({posts, title, remove}) => {
     return posts.length > 0
@@ -7,9 +8,17 @@ const PostList = ({posts, title, remove}) => {
             <h1 style={{textAlign: 'center'}}>
                 {title}
             </h1>
-            {posts.map((post, index) => 
-                <PostItem remove={remove} number={index} post={post} key={post.id}  />
+            <TransitionGroup>
+            {posts.map((post, index) =>
+                <CSSTransition
+                    timeout={500}
+                    key={post.id}
+                    classNames={"post"}
+                >
+                    <PostItem remove={remove} number={index} post={post}   />
+                </CSSTransition>
             )}
+            </TransitionGroup>
         </div>
     )
     : <h1>Not found any posts</h1>
