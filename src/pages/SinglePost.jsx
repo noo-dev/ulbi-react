fde66a2751be1c1938276ea45f8bebdc6ae40c3c
noo-dev <noo.dev23@gmail.com>
2024-01-12 09:49:19 +0500

    implemented fetching comments

--- src/pages/SinglePost.jsx
+++ src/pages/SinglePost.jsx
@@ -7,14 +7,22 @@ import Loader from '../components/UI/Loader/Loader'
 const SinglePost = () => {
     const params = useParams()
     const [post, setPost] = useState({})
+    const [comments, setComments] = useState([])
     const [fetchPostById, isLoading, postError] = useFetching(async (id) => {
         const response = await PostService.getById(id)
         setPost(response.data)
     })
 
+    const [fetchComments, isComLoading, comError] = useFetching(async (id) => {
+        const response = await PostService.getCommentsByPostId(id)
+        setComments(response.data)
+    })
+
     useEffect(() => {
         fetchPostById(params.id)
+        fetchComments(params.id)
     }, [])
+
 
     return (
         <div>
@@ -25,6 +33,18 @@ const SinglePost = () => {
             {isLoading
                 ? <Loader />
                 : <div>{post.id} {post.title}</div>
+            }
+            <h1>Coments</h1>
+            {isComLoading
+                ? <Loader /> 
+                : <div>
+                    {comments.map(comm => 
+                        <div style={{marginTop: 15}}>
+                            <h5>{comm.email}</h5>
+                            <div>{comm.body}</div>
+                        </div>
+                    )}
+                </div>
             }
         </div>
     )
